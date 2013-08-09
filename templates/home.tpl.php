0dad4e3f1dad1699437acf240c64a9e763fafae3
Joe Zhao <nikejaycn@gmail.com>
2013-08-09 13:39:56 +0800

    添加bootstrap rc3.01引用

--- templates/home.tpl.php
+++ templates/home.tpl.php
@@ -3,13 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <title>Jianshu RSS</title>
+    <link rel="stylesheet" href="http://cdn.staticfile.org/twitter-bootstrap/3.0.0-rc1/css/bootstrap.min.css">
 </head>
 <body>
 
-<form action="<?php echo $action; ?>" method="post">
-    <input type="text" id="url" name="url" value="">
-    <button type="submit">Go</button>
-</form>
+<div id="JSR-container">
+    <form action="<?php echo $action; ?>" method="post">
+        <input type="text" id="url" name="url" value="">
+        <button type="submit">Go</button>
+    </form>
+</div>
 
+<script type="text/javascript" src="http://cdn.staticfile.org/twitter-bootstrap/3.0.0-rc1/js/bootstrap.min.js"></script>
 </body>
 </html>
