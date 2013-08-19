06d8c89443c75df877108759c7d181986727234e
Jack Hu <hutushen222@gmail.com>
2013-08-19 08:15:57 +0800

    添加GA统计代码

--- templates/home.tpl.php
+++ templates/home.tpl.php
@@ -43,5 +43,16 @@
 </div>
 <script type="text/javascript" src="http://cdn.staticfile.org/jquery/2.0.3/jquery.min.js"></script>
 <script type="text/javascript" src="http://cdn.staticfile.org/twitter-bootstrap/3.0.0-rc1/js/bootstrap.min.js"></script>
+
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-43275645-1', 'milkythinking.com');
+    ga('send', 'pageview');
+
+</script>
 </body>
 </html>
