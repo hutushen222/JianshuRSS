ed2c2ec454c4e625ba8b0234e72486dfa4956a84
Jack Hu <hutushen222@gmail.com>
2013-08-29 23:58:52 +0800

    合并抓取文章的代码至独立的函数中

--- library/functions.php
+++ library/functions.php
@@ -10,3 +10,31 @@ function str_end_with($needle, $haystack) {
 
     return (substr($haystack, -$length) === $needle);
 }
+
+/**
+ * 抓取简书文章
+ *
+ * @param stdClass $note
+ */
+function fetchNote($note) {
+    $note_file_path = NOTES_ROOT . DS . md5($note->uri) . '.html';
+    if (file_exists($note_file_path)) {
+        $html_str = file_get_contents($note_file_path);
+    } else {
+        $html_str = file_get_contents(JIANSHU . $note->uri);
+        if ($html_str === false) {
+            throw new Exception('Can not fetch Jianshu note\'s content with uri: ' . $note->uri);
+        }
+        file_put_contents($note_file_path, $html_str);
+    }
+
+    $html = str_get_html($html_str);
+
+    $note->author = trim($html->find('.people .author', 0)->plaintext);
+    $note->author_uri = $html->find('.people .author', 0)->href;
+    $note->book = trim($html->find('.article .article-info a', 0)->plaintext);
+    $note->book_uri = $html->find('.article .article-info a', 0)->href;
+    $note->author_uri = $html->find('.people .author', 0)->href;
+    $note->body = $html->find('.show-content', 0)->innertext;
+    $note->created = trim($html->find('.article-info p', 0)->find('text', 3));
+}
--- public/index.php
+++ public/index.php
@@ -73,26 +73,7 @@ $app->get('/feeds/recommendations/notes', function () use ($app) {
     }
     // Notes
     foreach ($notes as $note) {
-        $note_file_path = NOTES_ROOT . DS . md5($note->uri) . '.html';
-        if (file_exists($note_file_path)) {
-            $html_str = file_get_contents($note_file_path);
-        } else {
-            $html_str = file_get_contents(JIANSHU . $note->uri);
-            if ($html_str === false) {
-                throw new Exception('Can not fetch Jianshu note\'s content with uri: ' . $note->uri);
-            }
-            file_put_contents($note_file_path, $html_str);
-        }
-
-        $html = str_get_html($html_str);
-
-        $note->author = trim($html->find('.people .author', 0)->plaintext);
-        $note->author_uri = $html->find('.people .author', 0)->href;
-        $note->book = trim($html->find('.article .article-info a', 0)->plaintext);
-        $note->book_uri = $html->find('.article .article-info a', 0)->href;
-        $note->author_uri = $html->find('.people .author', 0)->href;
-        $note->body = $html->find('.show-content', 0)->innertext;
-        $note->created = trim($html->find('.article-info p', 0)->find('text', 3));
+        fetchNote($note);
     }
 
     // Feed
@@ -141,26 +122,7 @@ $app->get('/feeds/collections/:id', function ($id) use ($app) {
 
     // Notes
     foreach ($notes as $note) {
-        $note_file_path = NOTES_ROOT . DS . md5($note->uri) . '.html';
-        if (file_exists($note_file_path)) {
-            $html_str = file_get_contents($note_file_path);
-        } else {
-            $html_str = file_get_contents(JIANSHU . $note->uri);
-            if ($html_str === false) {
-                throw new Exception('Can not fetch Jianshu note\'s content with uri: ' . $note->uri);
-            }
-            file_put_contents($note_file_path, $html_str);
-        }
-
-        $html = str_get_html($html_str);
-
-        $note->author = trim($html->find('.people .author', 0)->plaintext);
-        $note->author_uri = $html->find('.people .author', 0)->href;
-        $note->book = trim($html->find('.article .article-info a', 0)->plaintext);
-        $note->book_uri = $html->find('.article .article-info a', 0)->href;
-        $note->author_uri = $html->find('.people .author', 0)->href;
-        $note->body = $html->find('.show-content', 0)->innertext;
-        $note->created = trim($html->find('.article-info p', 0)->find('text', 3));
+        fetchNote($note);
     }
 
     // Feed
@@ -210,26 +172,7 @@ $app->get('/feeds/notebooks/:id', function ($id) use ($app) {
 
     // Notes
     foreach ($notes as $note) {
-        $note_file_path = NOTES_ROOT . DS . md5($note->uri) . '.html';
-        if (file_exists($note_file_path)) {
-            $html_str = file_get_contents($note_file_path);
-        } else {
-            $html_str = file_get_contents(JIANSHU . $note->uri);
-            if ($html_str === false) {
-                throw new Exception('Can not fetch Jianshu note\'s content with uri: ' . $note->uri);
-            }
-            file_put_contents($note_file_path, $html_str);
-        }
-
-        $html = str_get_html($html_str);
-
-        $note->author = trim($html->find('.people .author', 0)->plaintext);
-        $note->author_uri = $html->find('.people .author', 0)->href;
-        $note->book = trim($html->find('.article .article-info a', 0)->plaintext);
-        $note->book_uri = $html->find('.article .article-info a', 0)->href;
-        $note->author_uri = $html->find('.people .author', 0)->href;
-        $note->body = $html->find('.show-content', 0)->innertext;
-        $note->created = trim($html->find('.article-info p', 0)->find('text', 3));
+        fetchNote($note);
     }
 
     // Feed
@@ -279,26 +222,7 @@ $app->get('/feeds/users/:id', function ($id) use ($app) {
 
     // Notes
     foreach ($notes as $note) {
-        $note_file_path = NOTES_ROOT . DS . md5($note->uri) . '.html';
-        if (file_exists($note_file_path)) {
-            $html_str = file_get_contents($note_file_path);
-        } else {
-            $html_str = file_get_contents(JIANSHU . $note->uri);
-            if ($html_str === false) {
-                throw new Exception('Can not fetch Jianshu note\'s content with uri: ' . $note->uri);
-            }
-            file_put_contents($note_file_path, $html_str);
-        }
-
-        $html = str_get_html($html_str);
-
-        $note->author = trim($html->find('.people .author', 0)->plaintext);
-        $note->author_uri = $html->find('.people .author', 0)->href;
-        $note->book = trim($html->find('.article .article-info a', 0)->plaintext);
-        $note->book_uri = $html->find('.article .article-info a', 0)->href;
-        $note->author_uri = $html->find('.people .author', 0)->href;
-        $note->body = $html->find('.show-content', 0)->innertext;
-        $note->created = trim($html->find('.article-info p', 0)->find('text', 3));
+        fetchNote($note);
     }
 
     // Feed
