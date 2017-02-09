1e8e9630f7d0fe8c65599c21279c16de197b04f2
Jack Hu <hutushen222@gmail.com>
2017-02-09 12:06:49 +0800

    调整缓存清理脚本的日志输入
      - 添加日期
      - 调整格式

--- scripts/cache-cleaner.sh
+++ scripts/cache-cleaner.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-echo "Start cache cleaning ..."
+echo "$(date "+%Y-%m-%d %H:%M:%S")"
+echo "==> Start cache cleaning ..."
 
 DIR="$(readlink -m $0 | xargs dirname | xargs dirname)/storage/cache/$(date -d 'yesterday' '+%Y%m%d')/"
 
-echo "Dir is: $DIR"
+echo "==> DIR: $DIR"
 
 if [[ -d $DIR ]]; then
   if [[ -w $DIR ]]; then
     rm -rf $DIR
-    echo "Done"
+    echo "==> Done"
   else
-    echo "FAILED: permission deny"
+    echo "==> FAILED: permission deny"
   fi
 else
-  echo "FAILED: dir not exists"
+  echo "==> FAILED: dir not exists"
 fi
